fddcd65a603f4796298445ff87711498640ddd58
MbolaLiliane <mbola.liliane@gmail.com>
2023-01-17 19:18:14 +0100

    Commit

--- src/main/java/com/ve/ve/Controller/VenteEchereBack.java
+++ src/main/java/com/ve/ve/Controller/VenteEchereBack.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,75 +26,86 @@ public class VenteEchereBack {
     @Autowired
     private AdminLoginRepository user;
 
-    @Autowired 
+    @Autowired
     private CategorieRepository categorie;
 
-     /* LOGIN */
-     @RequestMapping(value = "/login/traitement", method = RequestMethod.POST, produces = "application/json")
-     @ResponseBody
-     @CrossOrigin
-     public Map<String, Object> login(HttpServletRequest request) {
-         Map<String, Object> map = new HashMap<>();
-         String status = null;
-         String retour = "";
-         String adminUser = request.getParameter("adminUser");
-         String pwd = request.getParameter("pwd");
-         System.out.println("adminUser : " + adminUser);
-         System.out.println("Mot de passe : " + pwd);
-         ArrayList<AdminLogin> resultat = user.getAdminLogin(adminUser, pwd);
-         for (int i = 0; i < resultat.size(); i++) {
-             System.out.println(resultat.get(i).getAdminUser());
-         }
-         if (resultat.size() == 1) {
-             HttpSession session = request.getSession();
-             map.put("iduser", resultat.get(0).getId());
-             session.setAttribute("idUser", resultat.get(0).getId());
-             retour = "Login correcte";
-             status = "succes";
-         } else {
-             retour = "Login incorrecte";
-             status = "erreur";
-         }
-         map.put("message", retour);
-         map.put("status", status);
-         return map;
-    }
-
-    @RequestMapping(value = "/incription", method = RequestMethod.POST,produces = "application/json")
+    /* LOGIN */
+    @RequestMapping(value = "/login/traitement", method = RequestMethod.POST, produces = "application/json")
     @ResponseBody
     @CrossOrigin
-    public Map<String,Object> inscriptionAdmin(HttpServletRequest request) {
-        Map<String,Object> map=new HashMap<>();
-        AdminLogin admin=new AdminLogin();
+    public Map<String, Object> login(HttpServletRequest request) {
+        Map<String, Object> map = new HashMap<>();
+        String status = null;
+        String retour = "";
+        String adminUser = request.getParameter("adminUser");
+        String pwd = request.getParameter("pwd");
+        System.out.println("adminUser : " + adminUser);
+        System.out.println("Mot de passe : " + pwd);
+        ArrayList<AdminLogin> resultat = user.getAdminLogin(adminUser, pwd);
+        for (int i = 0; i < resultat.size(); i++) {
+            System.out.println(resultat.get(i).getAdminUser());
+        }
+        if (resultat.size() == 1) {
+            HttpSession session = request.getSession();
+            map.put("iduser", resultat.get(0).getId());
+            session.setAttribute("idUser", resultat.get(0).getId());
+            retour = "Login correcte";
+            status = "succes";
+        } else {
+            retour = "Login incorrecte";
+            status = "erreur";
+        }
+        map.put("message", retour);
+        map.put("status", status);
+        return map;
+    }
+
+    @RequestMapping(value = "/incription", method = RequestMethod.POST, produces = "application/json")
+    @ResponseBody
+    @CrossOrigin
+    public Map<String, Object> inscriptionAdmin(HttpServletRequest request) {
+        Map<String, Object> map = new HashMap<>();
+        AdminLogin admin = new AdminLogin();
         admin.setAdminUser(request.getParameter("adminlogin"));
         admin.setPwd(request.getParameter("pwd"));
         user.insertAdminLogin(admin);
         return map;
     }
 
-    @RequestMapping(value = "/insertcategorie", method = RequestMethod.POST,produces = "application/json")
+    @RequestMapping(value = "/insertcategorie", method = RequestMethod.POST, produces = "application/json")
     @ResponseBody
     @CrossOrigin
     public void insertCategorie(HttpServletRequest request) {
-       Categorie cat=new Categorie();
-       cat.setCategorie(request.getParameter("categorie"));
-       categorie.insertCategorie(cat);
+        Categorie cat = new Categorie();
+        cat.setCategorie(request.getParameter("categorie"));
+        categorie.insertCategorie(cat);
     }
 
-
-    @RequestMapping(value = "/listecategorie", method = RequestMethod.GET,produces = "application/json")
+    @RequestMapping(value = "/listecategorie", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     @CrossOrigin
-    public Map<String,Object> listeCategorie(HttpServletRequest request) {
-        Map<String,Object> map=new HashMap<>();
-        map.put("categorie",categorie.getCategorie());
+    public Map<String, Object> listeCategorie(HttpServletRequest request) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("categorie", categorie.getCategorie());
         return map;
     }
 
+    /**************************************************************************************************************/
     @GetMapping("/index")
-    public String index(){
+    public String index() {
         return "index";
     }
- 
+
+    @GetMapping("/accueil")
+    public String accueil() {
+        return "accueil";
+    }
+
+    @GetMapping("/afficherListeCategorie")
+    public String afficherListeCategorie(Model model) {
+        ArrayList<Categorie> listeCategorie = categorie.getCategorie();
+        model.addAttribute("categorie", listeCategorie);
+        return "listeCategorie";
+    }
 
 }
