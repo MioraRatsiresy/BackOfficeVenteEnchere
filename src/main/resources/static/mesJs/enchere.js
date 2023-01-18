e5b578e504267e2c2934b4ba1e3c5fa89c23059c
MioraRatsiresy <miora.ratsiresy@gmail.com>
2023-01-18 07:00:38 +0300

    change by Mbola

---
+++ src/main/resources/static/mesJs/enchere.js
@@ -0,0 +1,68 @@
+function traitementLogin() {
+    console.log("Traitement login");
+    var email = document.getElementById("email").value;
+    var pwd = document.getElementById("pwd").value;
+    console.log("Identifiant=" + email + " & pwd=" + pwd);
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            retour = JSON.parse(this.responseText);
+            if (retour['message'] === "Login incorrecte") {
+                document.getElementById("message").innerHTML = retour['message'];
+            }
+            else {
+                window.location.replace("/accueil");
+            }
+        }
+    }
+    xmlhttp.open("POST", "http://localhost:4444/login/traitement?adminUser=" + email + "&pwd=" + pwd);
+    xmlhttp.send();
+}
+
+function listeCategorie() {
+    console.log("Liste categorie");
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            //document.getElementById("categorie").innerHTML=this.responseText;
+            const xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log("OK");
+                    document.getElementById("categorie").innerHTML = this.responseText;
+                }
+            }
+            xmlhttp.open("GET", "http://localhost:4444/afficherListeCategorie");
+            xmlhttp.send();
+        }
+    }
+    xmlhttp.open("GET", "http://localhost:4444/listecategorie");
+    xmlhttp.send();
+}
+
+function ajouterNouvelleCategorie() {
+    console.log("Nouvelle catégorie");
+    var categorie = document.getElementById("nomCategorie").value;
+    console.log("Nom=" + categorie);
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            //document.getElementById("categorie").innerHTML=this.responseText;
+            const xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log("OK");
+                    $('#exampleModal').modal('hide');
+                    document.getElementById("categorie").innerHTML = this.responseText;
+                }
+            }
+            xmlhttp.open("GET", "http://localhost:4444/afficherListeCategorie");
+            xmlhttp.send();
+        }
+    }
+    xmlhttp.open("POST", "http://localhost:4444/insertcategorie?categorie="+categorie);
+    xmlhttp.send();
+}
