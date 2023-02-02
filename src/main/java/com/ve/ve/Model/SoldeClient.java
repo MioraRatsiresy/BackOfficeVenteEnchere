d280c0baf34b5d28531b041518f32fe4646cfdb7
MbolaLiliane <mbola.liliane@gmail.com>
2023-02-02 11:13:24 +0100

    commit

---
+++ src/main/java/com/ve/ve/Model/SoldeClient.java
@@ -0,0 +1,38 @@
+package com.ve.ve.Model;
+
+/**
+ *
+ * @author Mbola
+ */
+public class SoldeClient {
+    private int clientid;
+    private double solde;
+
+    /**
+     * @return the clientid
+     */
+    public int getClientid() {
+        return clientid;
+    }
+
+    /**
+     * @param clientid the clientid to set
+     */
+    public void setClientid(int clientid) {
+        this.clientid = clientid;
+    }
+
+    /**
+     * @return the solde
+     */
+    public double getSolde() {
+        return solde;
+    }
+
+    /**
+     * @param solde the solde to set
+     */
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
+}
---
+++ src/main/java/com/ve/ve/Repository/CompteClientRepository.java
@@ -0,0 +1,29 @@
+package com.ve.ve.Repository;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.ve.ve.DAO.ClientDAO;
+import com.ve.ve.DAO.CompteClientDAO;
+import com.ve.ve.Model.Client;
+import com.ve.ve.Model.CompteClient;
+import com.ve.ve.Model.SoldeClient;
+
+@Repository
+public class CompteClientRepository implements CompteClientDAO {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public ArrayList<SoldeClient> getSolde(int idClient) {
+        String sql = "select clientid, sum(montant) as solde from compteclient where etat=0 and clientid=1 and actiontransaction=4 or etat=3 and clientid=1 and actiontransaction=4 group by clientid";
+        return (ArrayList<SoldeClient>) jdbcTemplate.query(sql,
+                new BeanPropertyRowMapper<SoldeClient>(SoldeClient.class));
+    }
+
+}
